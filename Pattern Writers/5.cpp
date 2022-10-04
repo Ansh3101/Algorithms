3f3ce46c6cb2aa5a38c33863f5aa80d03e8037fe
Anshuman Tekriwal <anshumantekriwalcode@gmail.com>
2022-10-04 23:23:42 +0530

    Added `180-Number Pyramid`

---
+++ Pattern Writers/5.cpp
@@ -0,0 +1,29 @@
+// 180 Number Pyramid
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Length Of Pyramid" << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+            if (j <= n - i)
+            {
+                cout << "  ";
+            }
+            else
+            {
+                cout << " " << i;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
