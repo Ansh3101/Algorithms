8e1c55bbf88cbb15d87089d7d4f78f365dca4939
Anshuman Tekriwal <anshumantekriwalcode@gmail.com>
2022-10-09 22:48:06 +0530

    `Floyd's Pyramid` Writer

---
+++ Pattern Writers/6.cpp
@@ -0,0 +1,23 @@
+// Floyd's Pyramid
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter Length Of Pyramid" << endl;
+    cin >> n;
+    int m = 1;
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << m << " ";
+            m++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
